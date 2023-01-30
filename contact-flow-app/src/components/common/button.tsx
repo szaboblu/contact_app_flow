b373d26d7b1ead9fd8fed310ea313aef8659492a
Balázs <szaboblu@gmail.com>
2023-01-30 04:00:24 +0100

    custom button

--- contact-flow-app/src/components/common/button.tsx
+++ contact-flow-app/src/components/common/button.tsx
@@ -5,29 +5,60 @@
 
 interface ButtonProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   type?: "button" | "submit" | "reset";
   icon?: React.ReactNode;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "special";
 }
 
-const Button = ({
+export const Button = ({
   label,
   onClick,
   type = "button",
   icon,
-  variant = "primary",
+  variant = "special",
 }: ButtonProps) => {
+  let buttonClass = "flex items-center  p-2  ";
+
+  if (icon && label) {
+    buttonClass += "px-3 gap-2 ";
+  }
+
+  switch (variant) {
+    case "primary":
+      buttonClass += "rounded-lg bg-G-60 hover:bg-G-50 active:bg-G-40";
+      break;
+    case "secondary":
+      buttonClass += "rounded-lg bg-G-100 hover:bg-G-90 active:bg-G-80 ";
+      break;
+    case "special":
+      const hover = "hover:from-G-60  hover:via-G-20 hover:bg-white/4";
+      const active = "active:from-G-60 active:via-G-20 active:bg-white/4";
+      const gradient = "from-G-60/70 to-G-60/100 from-G-20/100 to-G-20/0";
+      return (
+        <div className="items-center justify-center p-px mx-auto overflow-hidden rounded-full bg-gradient-to-b from-G-20/100 to-G-20/0">
+          <button
+            type={type}
+            onClick={onClick}
+            className={
+              buttonClass +
+              "bg-G-100 rounded-full h-full w-full bg-gradient-to-t from-G-60 to-G-20" +
+              gradient +
+              hover +
+              active
+            }
+          >
+            {icon && <span className="">{icon}</span>}
+            <span className="text-sm">{label}</span>
+          </button>
+        </div>
+      );
+  }
+
   return (
-    <button
-      type={type}
-      className={`button button--${variant} `}
-      onClick={onClick}
-    >
-      {icon && <span className="button__icon">{icon}</span>}
-      <span className="button__label">{label}</span>
+    <button type={type} className={buttonClass} onClick={onClick}>
+      {icon && <span className="">{icon}</span>}
+      <span className="text-sm">{label}</span>
     </button>
   );
 };
-
-export default Button;
